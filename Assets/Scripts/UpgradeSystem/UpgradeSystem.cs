cde11452f1bbc97fc83a6e8a38806ee64ebe0c6b
Trần Đình Vũ <53107088+trandinhvu13@users.noreply.github.com>
2020-04-11 22:19:27 +0700

    Done UpgradeSystem

--- Assets/Scripts/UpgradeSystem/UpgradeSystem.cs
+++ Assets/Scripts/UpgradeSystem/UpgradeSystem.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradeSystem : MonoBehaviour
-{
+public class UpgradeSystem : MonoBehaviour {
     public int maxMultiplierPriceStep;
     public int drinksMaxQuantityPriceStep;
     public int creamsMaxQuantityPriceStep;
@@ -15,40 +14,62 @@ public class UpgradeSystem : MonoBehaviour
     public int EALChancePriceStep;
     public int customerWaitingTimePriceStep;
 
-    void UpgradeMaxMulti()
-    {
+    void UpgradeMaxMulti () {
         PlayerStats.instance.maxMultiplier += 1;
         PlayerStats.instance.totalScore -= PlayerStats.instance.maxMultiplierPrice;
         PlayerStats.instance.maxMultiplierPrice += maxMultiplierPriceStep;
     }
-    void UgradeDrinksMaxQuan()
-    {
+    void UgradeDrinksMaxQuan () {
         PlayerStats.instance.drinkMQuantity += 1;
         PlayerStats.instance.totalScore -= PlayerStats.instance.drinksMaxQuantityPrice;
         PlayerStats.instance.drinksMaxQuantityPrice += drinksMaxQuantityPriceStep;
     }
-    void UpgradeCreamsMaxQuan()
-    {
+    void UpgradeCreamsMaxQuan () {
         PlayerStats.instance.creamMQuantity += 1;
         PlayerStats.instance.totalScore -= PlayerStats.instance.creamsMaxQuantityPrice;
         PlayerStats.instance.creamsMaxQuantityPrice += creamsMaxQuantityPriceStep;
     }
-    void UpgradeFruitsMaxQuan()
-    {
+    void UpgradeFruitsMaxQuan () {
         PlayerStats.instance.fruitMQuantity += 1;
         PlayerStats.instance.totalScore -= PlayerStats.instance.fruitsMaxQuantityPrice;
         PlayerStats.instance.fruitsMaxQuantityPrice += fruitsMaxQuantityPriceStep;
     }
 
-    void UpgradeMaxLife()
-    {
+    void UpgradeMaxLife () {
         PlayerStats.instance.maxLife += 1;
         PlayerStats.instance.totalScore -= PlayerStats.instance.maxLifePrice;
         PlayerStats.instance.maxLifePrice += maxMultiplierPriceStep;
     }
 
+    void UpgradeRestockTime () {
+        PlayerStats.instance.deliveryTime -= 1;
+        PlayerStats.instance.totalScore -= PlayerStats.instance.restockTimePrice;
+        PlayerStats.instance.restockCooldownPrice += restockCooldownPriceStep;
+    }
 
+    void UpgradeCooldownRestockTime () {
+        PlayerStats.instance.cooldownTime -= 1;
+        PlayerStats.instance.totalScore -= PlayerStats.instance.restockCooldownPrice;
+        PlayerStats.instance.restockCooldownPrice += restockCooldownPriceStep;
+    }
 
+    void UpgradeChanceVIP () {
+        PlayerStats.instance.VIPCustomerChance += 0.02f;
+        PlayerStats.instance.totalScore -= PlayerStats.instance.VIPChancePrice;
+        PlayerStats.instance.VIPChancePrice += VIPChancePriceStep;
+    }
+
+    void UpgradeChanceEAL () {
+        PlayerStats.instance.EALCustomerChance += 0.02f;
+        PlayerStats.instance.totalScore -= PlayerStats.instance.EALChancePrice;
+        PlayerStats.instance.EALChancePrice += EALChancePriceStep;
+    }
+
+    void UpgradeCustomerWaitingTime () {
+        PlayerStats.instance.customerWaitingTime += 1;
+        PlayerStats.instance.totalScore -= PlayerStats.instance.customerWaitingTimePrice;
+        PlayerStats.instance.customerWaitingTimePrice += customerWaitingTimePriceStep;
+    }
 
 
 }
